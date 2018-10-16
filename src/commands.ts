bf1c56a474c41ef7408cc1a0ea48b6c9a2043e63
Ozair Patel <ozairpatel2@gmail.com>
2018-10-16 17:49:17 -0500

    Remove url from command regex

--- src/commands.ts
+++ src/commands.ts
@@ -4,7 +4,7 @@ import { summarizeTeam, teamSummaryToMessage } from './functions/summarizer'
 export const commands: [[RegExp, (...args: Array<any>) => Promise<StringResolvable | [StringResolvable, MessageOptions]>]] = [
 
 	[
-		/\.bb summarize (https:\/\/compete\.tespa\.org\/tournament\/111\/team\/\d+(?:\/)?)/,
+		/\.bb summarize (.+)/,
 		async function (teamPage: string) {
 			const summary = await summarizeTeam(teamPage)
 			return teamSummaryToMessage(summary)
